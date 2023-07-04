a93ebd7d4d40cf57dfaad065ee9a9909e4892d07
MaxitoMix <128189550+MaxitoMix@users.noreply.github.com>
2023-07-04 18:49:21 -0400

    done

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,18 @@
+import storeItems from "../data/items.json"
+
 export function Store(){
     return(
+        <>
         <h1>Store</h1>
+        <div className="storeContainer">
+            {storeItems.map(item => (
+                <div className="item">{JSON.stringify(item)}</div>
+            ))
+            }
+        </div>
+        </>
     )
 }
+
+// 1 hour mark
+// https://www.youtube.com/watch?v=lATafp15HWA&ab_channel=WebDevSimplified
