53591857b5a03de481c342c229c5ebba385eb6fd
MaxitoMix <128189550+MaxitoMix@users.noreply.github.com>
2023-07-10 13:00:41 -0400

    done

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/maxitomix/top_shopping_cart',
+  base: '/top_shopping_cart/',
   plugins: [react()],
 })
